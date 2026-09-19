ddb6bab51b78b91974a950619a4856542ced1e6c
karloss-1 <jbw89cvbd9@privaterelay.appleid.com>
2026-09-18 20:30:55 -0600

    Improve FSRS rating guidance labels

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "mexican-spanish-flashcards-v4";
+const CACHE_NAME = "mexican-spanish-flashcards-v5";
 const APP_FILES = [
   "./", "./index.html", "./manifest.webmanifest", "./data/decks.js", "./study-policy.js",
   "./install.js",
